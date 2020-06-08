d0fce92d2e86e95b6b85ec8cefdfc4f01532fdb0
ibrahim elbatal <ibrahimelbatal2@gmail.com>
2020-06-08 19:49:53 +0200

    DatePicker and Customize DropDownList and adding Date Template

---
+++ StudentTask/StudentTask/Scripts/app/app.js
@@ -0,0 +1,27 @@
+﻿function CustomizeDropDownList(url) {
+
+    var governorateDDL = $('.GovernorateDropDown');
+    var neighborhoodDDL = $('.NeighborhoodDropDown');
+    var ddlOptions = '';
+    governorateDDL.on('change',
+        function() {
+            var value = Number($(this).val());
+            ddlOptions = '<option value>Select Neighborhood</option>';
+            if (value === 1) {
+                neighborhoodDDL.empty();
+                $.getJSON(url,
+                    function(data) {
+                        $.each(data,
+                            function(index, item) {
+                                ddlOptions += '<option value="' + item.ID + '">' + item.Name + '</option>';
+                            });
+                        neighborhoodDDL.append(ddlOptions);
+                    });
+            } else {
+                console.log("jjj");
+                neighborhoodDDL.empty();
+                neighborhoodDDL.append(ddlOptions);
+
+            }
+        });
+}
---
+++ StudentTask/StudentTask/Scripts/app/datepicker.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('.date').datepicker({
+        dateformat: 'MM/dd/yyy',
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "1990:2020"
+
+        
+    });
+});
