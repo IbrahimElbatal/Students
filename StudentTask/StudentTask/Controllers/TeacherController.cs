7341efeb74025e401fc868b903ce38da14e023e6
ibrahim elbatal <ibrahimelbatal2@gmail.com>
2020-06-10 11:24:44 +0200

    adding teacher controller for adding a teacher and list all teachers

---
+++ StudentTask/StudentTask/Controllers/TeacherController.cs
@@ -0,0 +1,46 @@
+﻿using StudentTask.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StudentTask.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly StudentEntities db;
+        public TeacherController()
+        {
+            db = new StudentEntities();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Index()
+        {
+            var teachers = db.Teachers.ToList();
+
+            return View(teachers);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Teacher teacher)
+        {
+            if (!ModelState.IsValid)
+                return View(teacher);
+
+            db.Teachers.Add(teacher);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StudentTask/StudentTask/Models/TeacherMetaData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentTask.Models
+{
+    [MetadataType(typeof(TeacherMetaData))]
+    public partial class Teacher
+    {
+
+    }
+    public class TeacherMetaData
+    {
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Birth Date")]
+        [UIHint("Date")]
+        public DateTime BirthDate { get; set; }
+
+    }
+}
